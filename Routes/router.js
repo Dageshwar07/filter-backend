8825fcf1ba256f2a51e46d264c091159e8919067
Dageshwar Das <dageshwardasmanikpuri996@gmail.com>
2023-03-02 13:18:36 +0530

    hello

--- Routes/router.js
+++ Routes/router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = new express.Router();
-const controllers = require("../Controllers/usersControllers");
+const controllers = require("../controllers/usersControllers");
 const upload = require("../multerconfig/storageConfig")
 
 
